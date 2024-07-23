b1014621a6d93233728911e57be7f9a719baea95
diazarm <marceloardiaz@gmail.com>
2024-07-22 22:02:41 -0400

    first commit with php

---
+++ basico_php.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "hola mundo\n";
+
+//variables
+
+$my_string = "esto es una cadema de texto" . "\n";
+
+echo $my_string;
+// para concatenar puedo colocar el . en vez de +
+//el metodo gettype() me dice el tipo de dato
+
+echo "el tipo de dato es " . gettype($my_string);
+
+
